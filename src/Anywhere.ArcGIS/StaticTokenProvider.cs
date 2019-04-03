e9ea8a67e684762862858d71fd6bde0f6f303b9d
Heidi Binder-Vitti <heidi@dymaptic.com>
2019-04-03 12:53:46 -0700

    Adding StaticTokenProvider class

---
+++ src/Anywhere.ArcGIS/StaticTokenProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Anywhere.ArcGIS.Operation;
+
+namespace Anywhere.ArcGIS
+{
+    public class StaticTokenProvider : ITokenProvider, IDisposable
+    {
+        public string RootUrl { get; set; }
+
+        public ISerializer Serializer { get; set; }
+
+        public ICryptoProvider CryptoProvider { get; set; }
+
+        public string UserName { get; set; }
+
+        private Token token { get; set; }
+
+        public StaticTokenProvider(string rootURL, string token)
+        {
+            this.RootUrl = rootURL.AsRootUrl();
+            CryptoProvider = CryptoProviderFactory.Get();
+            UserName = "";
+            Serializer = SerializerFactory.Get();
+            this.token = new Token() { Value = token };
+        }
+
+        ~StaticTokenProvider()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.token = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public Task<Token> CheckGenerateToken(CancellationToken ct)
+        {
+            return Task.Run(() => this.token);
+        }
+    }
+}
